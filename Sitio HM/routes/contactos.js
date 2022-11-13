4aa5f6111cffee0b53c7c3c31175cd0a3028cfae
Julian Gonzalez <gonzalezjulian501@gmail.com>
2022-11-13 10:58:28 -0300

    agregamos mailtrap a contactos.js

--- Sitio HM/routes/contactos.js
+++ Sitio HM/routes/contactos.js
@@ -1,34 +1,38 @@
 var express = require('express');
 var router = express.Router();
+ var nodemailer = require("nodemailer");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('contactos', {
-    isContactos:true   } );
+    isContactos:true   
+  });//contactos.hbs
   });
   
-  var  nodemailer= requiere("nodemailer");
-  router.post("/",async(req,res,next)=>{
+ 
+  router.post("/", async(req,res,next)=> {
+// console.log(req.body);
+    var nombre= req.body.nombre;
+    var email= req.body.email;
+    var tel= req.body.tel;
+    var mensaje= req.body.mensaje;
 
-    var nombre=req.body.nombre;
-    var email=req.body.email;
-    var tel=req.body.tel;
-    var mensaje=req.body.mensaje;
     var obj= {
       to:"gonzalezjulian501@gmail.com",
       subject:"Contacto Web",
-      html:nombre + 'se contacto a traves de la web y quiere más información a este correo: '+email+ ". <br> Además , hizo este comentario:"+ mensaje+".<br> Su tel es :"+ tel
+      html:nombre + ' se contacto a traves de la web y quiere más información a este correo: '+email+ ". <br> Además , hizo este comentario: "+ mensaje+".<br> Su tel es :"+ tel
     }
-    var transport = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      auth:{
-        user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PAST,
-    }
-    
-  });
-  res.render=('contacto',{
+var transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  }
+});
+var info= await transporter.sendMail(obj);
+  res.render=('contactos',{
+    isContactos:true,
     message:'Mensaje enviado correctamente'
   });
 });
